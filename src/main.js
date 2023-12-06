8fd1aeb3f73552f707dab4becb7ba994ea3841ec
josRod128 <jose12836@gmail.com>
2023-12-05 23:24:15 -0700

    [add] ElementPlusIconsVue y axios

--- src/main.js
+++ src/main.js
@@ -5,9 +5,22 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import axios from 'axios';
+const axiosInstance = axios.create({
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    }
+});
+const envRoute = import.meta.env.VITE_ROUTE_BACK
 
 const app = createApp(App)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.config.globalProperties.$axios = { ...axiosInstance };
+app.config.globalProperties.$envRoute = envRoute;
 app.use(ElementPlus)
 app.use(router)
 
